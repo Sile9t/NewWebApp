85076dfeff6ed51b7629ed29c676ae9f7f0b5199
Sile9t <eyesless1jack@gmail.com>
2024-06-21 16:07:09 +0300

    Added Query class

---
+++ NewWebApp/Graph/Query/Query.cs
@@ -0,0 +1,13 @@
+﻿using NewWebApp.Abstractions;
+using NewWebApp.Dto;
+using NewWebApp.Models;
+
+namespace NewWebApp.Graph.Query
+{
+    public class Query (IProductRepository productRepository)
+    {
+        public IEnumerable<ProductDto> GetProducts() => productRepository.GetAllProducts();
+        public IEnumerable<ProductGroupDto> GetProductGroups([Service] ProductGroupRepository groupRepository) 
+            => groupRepository.GetAllProductGroups();
+    }
+}
