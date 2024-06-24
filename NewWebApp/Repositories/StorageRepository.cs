ce10a6e9bdf6c196074e244c52b04d5f24c17534
Sile9t <eyesless1jack@gmail.com>
2024-06-24 12:41:31 +0300

    Added StorageRepository class

---
+++ NewWebApp/Repositories/StorageRepository.cs
@@ -0,0 +1,33 @@
+﻿using AutoMapper;
+using Microsoft.Extensions.Caching.Memory;
+using NewWebApp.Dto;
+
+namespace NewWebApp.Repositories
+{
+    public class StorageRepository : IStorageRepository
+    {
+        private readonly IMapper _mapper;
+        private readonly IMemoryCache _cache;
+
+        public StorageRepository(IMapper mapper, IMemoryCache cache)
+        {
+            _mapper = mapper;
+            _cache = cache;
+        }
+
+        public int AddStorage(StorageDto storage)
+        {
+            throw new NotImplementedException();
+        }
+
+        public StorageDto DeleteStorage(string name)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<StorageDto> GetStorages()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
--- NewWebApp/obj/Debug/net8.0/NewWebApp.AssemblyInfo.cs
+++ NewWebApp/obj/Debug/net8.0/NewWebApp.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("NewWebApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+41be74fb7057292796a6d5ae4023f4ff50d012c9")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ba7f1812d15e03b7e935bee67f4a213e6df411ea")]
 [assembly: System.Reflection.AssemblyProductAttribute("NewWebApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("NewWebApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
