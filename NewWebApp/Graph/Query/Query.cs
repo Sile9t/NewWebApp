8a560a340075ac715504b2480a82223ad2b746e1
Sile9t <eyesless1jack@gmail.com>
2024-06-24 13:31:47 +0300

    AddedGetStorages in Query class

--- NewWebApp/Graph/Query/Query.cs
+++ NewWebApp/Graph/Query/Query.cs
@@ -6,8 +6,13 @@ namespace NewWebApp.Graph.Query
 {
     public class Query (IProductRepository productRepository)
     {
-        public IEnumerable<ProductDto> GetProducts() => productRepository.GetAllProducts();
+        public IEnumerable<ProductDto> GetProducts()
+            => productRepository.GetAllProducts();
+
         public IEnumerable<ProductGroupDto> GetProductGroups([Service] IProductGroupRepository groupRepository) 
             => groupRepository.GetAllProductGroups();
+
+        public IEnumerable<StorageDto> GetStorages([Service] IStorageRepository storageRepository)
+            => storageRepository.GetStorages();
     }
 }
